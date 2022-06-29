a770461a632bd26da23eb638bf97b9876ef2884d
George clark <george_clark@Georges-MacBook-Pro-2.local>
2022-06-29 13:48:02 +0100

    first commit

--- components/sharedstyles.tsx
+++ components/sharedstyles.tsx
@@ -1,14 +1,27 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const Container = styled.div`
-  padding: 0 0.5rem;
+const SiteWrap = styled.div`
   display: flex;
   flex-flow: column nowrap;
   justify-content: center;
   align-items: center;
   height: 100vh;
   min-height: 100vh;
-`
+
+  background: ${({ theme }) => theme.colors.primary};
+`;
+
+const Container = styled.div`
+  padding: 0 0.5rem;
+  background white;
+  max-width: 40rem;
+  /* display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  min-height: 100vh; */
+`;
 const Main = styled.main`
   padding: 5rem 0;
   flex: 1;
@@ -16,7 +29,7 @@ const Main = styled.main`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const Title = styled.h1`
   margin: 0;
@@ -34,13 +47,13 @@ const Title = styled.h1`
       text-decoration: underline;
     }
   }
-`
+`;
 
 const Description = styled.p`
   text-align: center;
   line-height: 1.5;
   font-size: 1.5rem;
-`
+`;
 const CodeTag = styled.code`
   background: #fafafa;
   border-radius: 5px;
@@ -49,6 +62,6 @@ const CodeTag = styled.code`
   font-size: 1.1rem;
   font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
     Bitstream Vera Sans Mono, Courier New, monospace;
-`
+`;
 
-export { Container, Main, Title, Description, CodeTag }
+export { SiteWrap, Container, Main, Title, Description, CodeTag };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,26 @@
-import type { AppProps } from 'next/app'
-import { ThemeProvider, DefaultTheme } from 'styled-components'
-import GlobalStyle from '../components/globalstyles'
+import type { AppProps } from "next/app";
+import { ThemeProvider, DefaultTheme } from "styled-components";
+import GlobalStyle from "../components/globalstyles";
+import { SiteWrap } from "../components/sharedstyles";
 
 const theme: DefaultTheme = {
   colors: {
-    primary: '#111',
-    secondary: '#0070f3',
+    bg: "#333",
+    surface: "#eee",
+    primary: "#333",
+    secondary: "gold",
   },
-}
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <Component {...pageProps} />
+        <SiteWrap>
+          <Component {...pageProps} />
+        </SiteWrap>
       </ThemeProvider>
     </>
-  )
+  );
 }
