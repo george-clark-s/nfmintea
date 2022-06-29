dc71231168883c0e3a9cd161f0d80613ca0f78dc
George clark <george_clark@Georges-MacBook-Pro-2.local>
2022-06-29 13:54:20 +0100

    delete theme ts stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,16 @@
-import type { AppProps } from "next/app";
-import { ThemeProvider, DefaultTheme } from "styled-components";
+import { ThemeProvider } from "styled-components";
 import GlobalStyle from "../components/globalstyles";
 import { SiteWrap } from "../components/sharedstyles";
 
-const theme: DefaultTheme = {
+const theme = {
   colors: {
-    bg: "#333",
     surface: "#eee",
     primary: "#333",
     secondary: "gold",
   },
 };
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return (
     <>
       <ThemeProvider theme={theme}>
